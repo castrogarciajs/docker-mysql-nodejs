d1705e6dd746137ac72ffb84433af52d09f8a663
sebastian009w <ia3266505@gmail.com>
2023-07-30 19:58:26 -0500

    chore: docker compose mysql

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import { createPool } from "mysql2";
 
 const app = express();
-app.get("/", (req, res) => {
+
+const pool = createPool({
+  host: "localhost",
+  user: "root",
+  password: "root123",
+  port: "3306",
+});
+
+app.get("/", async (req, res) => {
+  const query = pool.query("SELECT NOW()");
+
   res.json({
     message: "docker images",
+    query: query[0],
   });
 });
 
